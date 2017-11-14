10d6e980956775f281a40db362df76f1af8a61bb
Cesar <cesar777welch@gmail.com>
2017-11-13 21:43:25 -0600

    v2: added gen

--- compiler/src/finalCode/finalCodeExec.java
+++ compiler/src/finalCode/finalCodeExec.java
@@ -4,7 +4,6 @@ package finalCode;
  *
  * @author cwelch
  */
-
 import analysis.PascalSyn;
 import analysis.SemTypeChecker;
 import analysis.SemNameResolver;
@@ -25,28 +24,58 @@ import org.jdom.Document;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
-
 public class finalCodeExec {
 
-	/** La secuencia de código.  */
-	public static LinkedList<ImcBlock> chunks;
+    /**
+     * La secuencia de código.
+     */
+    public static LinkedList<ImcBlock> chunks;
 
-	/**
-	 *  se empieza la traduccion hasta la fase de analisis semantico
-	 */
-	public static void exec() {
-		/* se abre el archivo de entrada */
-		SAXBuilder builder = new SAXBuilder();
-		String xml_path = "imcode.xml";
-		Document xml_doc = null;
-            try {
-                xml_doc = builder.build(xml_path); //builder.build construye un arbol
-            } catch (JDOMException ex) {
-                Logger.getLogger(finalCodeExec.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-                Logger.getLogger(finalCodeExec.class.getName()).log(Level.SEVERE, null, ex);
+    /**
+     * se empieza la traduccion hasta la fase de analisis semantico
+     */
+    public static void exec() {
+        /* se abre el archivo de entrada */
+        SAXBuilder builder = new SAXBuilder();
+        String xml_path = "imcode.xml";
+        Document xml_doc = null;
+        try {
+            xml_doc = builder.build(xml_path); //builder.build construye un arbol
+        } catch (JDOMException ex) {
+            Logger.getLogger(finalCodeExec.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(finalCodeExec.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Element root = xml_doc.getRootElement();
+        List list = root.getChildren("datachunk");
+
+        for (int i = 0; i < list.size(); i++) {
+            Element node = (Element) list.get(i);
+            String suffix = "";
+            switch (node.getAttribute("size").getValue()) {
+                case "4":
+                    suffix = ".word 0";
+                break;
+                default: 
+                    suffix = ".space 20";
+                break;
             }
-		Element root = xml_doc.getRootElement();
-                System.out.println(root.getAttributes());
-	}
+            
+             System.out.println(node.getAttribute("label").getValue() + " : " + suffix);
+
+        }
+
+//        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+//                new FileOutputStream("finalCode.asm"), "utf-8"))) {
+//            writer.write(".data");
+//            newLine(writer);
+//            writer.write("test");
+//        } catch (IOException ex) {
+//            Logger.getLogger(finalCodeExec.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+
+    public static void newLine(Writer writer) throws IOException {
+        writer.write("\n");
+    }
 }
